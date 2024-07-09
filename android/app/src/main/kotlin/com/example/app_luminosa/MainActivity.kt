a6f250718834ea5c3a8aad30b8a01dfc8b32c8cc
luskasouza <zerolucas11@gmail.com>
2024-07-09 14:11:23 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/app_luminosa/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.app_luminosa
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/utils/constants.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+const primaryColor = Color(0xFF6B8FC4);
+const whiteColor = Colors.white;
